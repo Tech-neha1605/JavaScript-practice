1534386518328121abaf9625550ae1e39d59058a
Neha Patil <tech.neha1605@gmail.com>
2025-10-27 14:26:48 +0530

    Faltten Array

---
+++ problem10_flattenArray.js
@@ -0,0 +1,16 @@
+function flatten(arr) {
+  const res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      const flat = flatten(arr[i]);
+      for (let j = 0; j < flat.length; j++) res.push(flat[j]);
+    } else {
+      res.push(arr[i]);
+    }
+  }
+  return res;
+}
+
+// Sample
+console.log(flatten([1, [2, [3,4], 5], 6])); // [1,2,3,4,5,6]
+module.exports = { flatten };
